0c6ff9cfa6510016c623c88ecf08d558d9f1f314
IlMelons <melonss2002@gmail.com>
2025-09-16 11:10:20 +0200

    [mnr_postals] Code Rework

--- server/server.lua
+++ server/server.lua
@@ -4,8 +4,8 @@ lib.addCommand(config.command, {
     help = locale('help_command'),
     params = {
         {
-            name = "postal",
-            type = "number",
+            name = 'postal',
+            type = 'number',
             help = locale('help_param'),
         },
     },
@@ -13,6 +13,6 @@ lib.addCommand(config.command, {
 }, function(source, args, raw)
     local postalcode = args.postal
     if postalcode then
-        TriggerClientEvent("mnr_postals:client:SetWaypoint", source, postalcode)
+        TriggerClientEvent('mnr_postals:client:SetWaypoint', source, postalcode)
     end
 end)
